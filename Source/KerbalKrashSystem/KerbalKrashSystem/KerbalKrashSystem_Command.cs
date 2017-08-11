1d6a0ce5c966ec3c5ba088eb026eb3f4b3c8c319
EnzoMeertens <EnzoMeertens@users.noreply.github.com>
2017-08-11 14:34:07 +0200

    Fixed Kerbals leaking. Updated ModuleManager. Added collider subdivisions.

--- Source/KerbalKrashSystem/KerbalKrashSystem/KerbalKrashSystem_Command.cs
+++ Source/KerbalKrashSystem/KerbalKrashSystem/KerbalKrashSystem_Command.cs
@@ -4,9 +4,26 @@ namespace KKS
 {
     public class ModuleKerbalKrashSystem_Command : KerbalKrashSystem
     {
+        #region Variables
+        #region Protected fields
+        [KSPField(guiName = "Hatch malfunction", guiActive = false)]
+        public bool _hatchMalfunction = true;
+        /// <summary>
+        /// Boolean value indicating if the hatch can malfunction (get stuck) and trap Kerbals inside.
+        /// </summary>
+        protected bool HatchMalfunction
+        {
+            get { return _hatchMalfunction; }
+            set { _hatchMalfunction = value; }
+        }
+        #endregion
+
+        #region Private fields
         private bool _wasDamaged = false;
 
         private GameObject _blockage = null;
+        #endregion
+        #endregion
 
         protected override void OnEnabled()
         {
@@ -29,15 +46,22 @@ namespace KKS
             if (damage < DamageThreshold || _wasDamaged || part.airlock == null)
                 return;
 
-            //Add an airlock blockage to prevent Kerbals from exiting.
-            _blockage = new GameObject("blockage");
-            _blockage.AddComponent<BoxCollider>().isTrigger = true;
-            _blockage.AddComponent<Rigidbody>().isKinematic = false;
-            _blockage.transform.parent = part.airlock;
-            _blockage.transform.position = part.airlock.position;
-            _blockage.transform.rotation = part.airlock.rotation;
-            _blockage.AddComponent<FixedJoint>().connectedBody = part.Rigidbody;
-            _blockage.transform.localScale = new Vector3(0.25f, 0.25f, 0.05f);
+            if (HatchMalfunction)
+            {
+                //Add an airlock blockage to prevent Kerbals from exiting.
+                _blockage = new GameObject("blockage");
+
+                _blockage.AddComponent<BoxCollider>().isTrigger = true;
+
+                //Has to be rigidbody to trigger the collider trigger.
+                _blockage.AddComponent<Rigidbody>().isKinematic = false;
+
+                _blockage.transform.parent = part.airlock;
+                _blockage.transform.position = part.airlock.position;
+                _blockage.transform.rotation = part.airlock.rotation;
+                _blockage.AddComponent<FixedJoint>().connectedBody = part.Rigidbody;
+                _blockage.transform.localScale = new Vector3(0.25f, 0.25f, 0.05f);
+            }
 
             _wasDamaged = true;
 
@@ -50,7 +74,8 @@ namespace KKS
                 return;
 
             //Get rid of airlock blockage.
-            Destroy(_blockage);
+            if (HatchMalfunction)
+                Destroy(_blockage); 
 
             _wasDamaged = false;
 
